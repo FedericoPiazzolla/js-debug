f6b98e521063ac0285856329dafd4b46123b7b1e
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-10 16:10:23 +0100

    add: end bundle 2

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -22,23 +22,37 @@ function checkAge() {
     }
 }
 checkAge();
+//message è assegnata come costante, quindi non potrà cambiare
+// SOLUZIONE
+function checkAge() {
+    const myAge = 32;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+};
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+    console.log(colors.length);
 }
 printColorsNumber();
-
+// errore di scrittura di lenght invece di length
 
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero'));
     const total = userNumber + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+//riga 49 serviva il parsint per non far concatenare la stringa ed il numero
 
 
 // ESERCIZIO 4
@@ -46,10 +60,10 @@ function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -59,6 +73,7 @@ function checkAccess() {
     }
 }
 checkAccess();
+// i valori booleani sono stringhe, e quindi darà sempre l'accesso negato
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -88,6 +103,9 @@ function checkAccessImproved() {
         }
     }
     checkAccessImproved();
+};
+
+// mancava la chiusura della funzione
 
 
 
